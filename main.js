a734ccd2b18e02bd47a2ab75619f38fd0e02e841
abdullah-sameh <abdullahmaher@std.mans.edu.eg>
2023-01-10 22:37:50 +0200

    setting page and some editing in dashboard page

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const navLinks = document.querySelectorAll('#navLink')
 
+// for display hove effect on a link that reffer to current page
 for (const link of navLinks) {
-  if (link.innerHTML === document.title) {
-
+  if (link.innerText.split('\n')[1] === document.title) {
     link.parentElement.classList.add('hover')
   }
 }
